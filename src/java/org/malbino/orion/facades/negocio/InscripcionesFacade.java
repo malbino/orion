b35b7c9cb50f68169490d0ba331addad07ec6846
Martin Albino Ascui <maacrazy@gmail.com>
2019-12-26 23:57:10 -0400

    Se añadio Cambio Carrera

--- src/java/org/malbino/orion/facades/negocio/InscripcionesFacade.java
+++ src/java/org/malbino/orion/facades/negocio/InscripcionesFacade.java
@@ -123,6 +123,38 @@ public class InscripcionesFacade {
         return true;
     }
 
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public boolean cambioCarrera(Estudiante estudiante, Carrera carrera, GestionAcademica gestionAcademica) {
+        Date fecha = Fecha.getDate();
+        long c1 = inscritoFacade.cantidadInscritos(gestionAcademica.getId_gestionacademica(), carrera.getId_carrera());
+        String matricula = gestionAcademica.getGestion().toString() + gestionAcademica.getPeriodo().getPeriodoEntero().toString() + carrera.getId_carrera() + String.format("%04d", (c1 + 1));
+        Inscrito inscrito = new Inscrito(fecha, Tipo.NUEVO, matricula, estudiante, carrera, gestionAcademica);
+        em.persist(inscrito);
+
+        if (carrera.getCampus().getInstituto().getCaracter().equals(Caracter.CONVENIO) || carrera.getCampus().getInstituto().getCaracter().equals(Caracter.PUBLICO)) {
+            Integer monto = carrera.getCreditajeMatricula() * carrera.getCampus().getInstituto().getPrecioCredito();
+
+            Pago pago = new Pago(Concepto.MATRICULA, monto, false, inscrito);
+            em.persist(pago);
+        } else {
+            Long creditajeMaterias = materiaFacade.creditajeMaterias(carrera.getId_carrera(), carrera.getRegimen().equals(Regimen.SEMESTRAL) ? Nivel.PRIMER_SEMESTRE : Nivel.PRIMER_AÑO);
+            Integer monto = creditajeMaterias.intValue() * carrera.getCampus().getInstituto().getPrecioCredito();
+            Integer cuotas = carrera.getRegimen().getCuotas();
+            for (Concepto concepto : Concepto.values(carrera.getRegimen())) {
+                Double montoCuotaSinRedondear = monto.doubleValue() / cuotas.doubleValue();
+                Integer montoCuotaRedondeado = Redondeo.redondear_UP(montoCuotaSinRedondear, 0).intValue();
+
+                Pago pago = new Pago(concepto, montoCuotaRedondeado, false, inscrito);
+                em.persist(pago);
+
+                monto -= montoCuotaRedondeado;
+                cuotas--;
+            }
+        }
+
+        return true;
+    }
+
     @Transactional(Transactional.TxType.REQUIRED)
     public Long creditajeOferta(Inscrito inscrito) {
         Long l = 0L;
